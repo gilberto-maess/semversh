f012a0cc40a4bf64458bd6e6dea1ddb549ec495e
gilberto-maess <gilberto@maess.com.br>
2023-12-01 15:43:13 -0300

    feat: script shell para obter semantic version

---
+++ script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ -z "$GIT_HASH" ]; then
+  echo "Erro: A variável \$GIT_HASH não foi informada."
+  exit 1
+fi
+
+MAJOR=0
+MINOR=0
+PATCH=0
+
+COMMITS=$(git log --oneline $GIT_HASH..HEAD)
+
+while read -r commit; do
+  TIPO=$(echo "$commit" | awk '{print $2}')
+  case "$TIPO" in
+    "fix:")
+      ((PATCH++))
+      ;;
+    "patch:")
+      ((PATCH++))
+      ;;
+    "feat:")
+      ((MINOR++))
+      ;;
+    "minor:")
+      ((MINOR++))
+      ;;
+    "major:")
+      ((MAJOR++))
+      ;;
+    "break:")
+      ((MAJOR++))
+      ;;
+    *)
+      ;;
+  esac
+done <<< "$COMMITS"
+
+echo "$MAJOR.$MINOR.$PATCH"
